e87f794e97701a4185ec66b403234030bb712c48
gift-minwo <gift@minwo.co>
2022-05-28 12:15:07 +0100

    side and top navbar created

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import "./Sidebar.scss"
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 const Sidebar = () => {
    return (
       <div className="sidebar">
@@ -7,20 +19,68 @@ const Sidebar = () => {
                reactadmin
             </span>
          </div>
+         <hr/>
          <div className="center">
             <ul>
+               <p className="title">MAIN</p>
                <li>
-                  <spna>Dashboard</spna>
+                  <DashboardIcon className="icon" />
+                  <span>Dashboard</span>
+               </li>
+               <p className="title">LISTS</p>
+               <li>
+                  <PersonOutlineIcon className="icon" />
+                  <span>Users</span>
                </li>
                <li>
-                  <spna>Dashboard</spna>
+                  <StoreIcon className="icon" />
+                  <span>Products</span>
                </li>
                <li>
-                  <spna>Dashboard</spna>
+                  <CreditCardIcon className="icon" />
+                  <span>Order</span>
+               </li>
+               <li>
+                  <LocalShippingIcon className="icon" />
+                  <span>Delivery</span>
+               </li>
+               <p className="title">USEFUL</p>
+               <li>
+                  <InsertChartIcon className="icon" />
+                  <span>Stats</span>
+               </li>
+               <li>
+                  <NotificationsNoneIcon className="icon" />
+                  <span>Notification</span>
+               </li>
+               <p className="title">SERVICE</p>
+               <li>
+                  <SettingsSystemDaydreamOutlinedIcon className="icon" />
+                  <span>System Health</span>
+               </li>
+               <li>
+                  <PsychologyOutlinedIcon className="icon" />
+                  <span>Logs</span>
+               </li>
+               <li>
+                  <SettingsApplicationsIcon className="icon" />
+                  <span>Settings</span>
+               </li>
+               <p className="title">USER</p>
+               <li>
+                  <AccountCircleOutlinedIcon className="icon" />
+                  <span>Profile</span>
+               </li>
+               <li>
+                  <ExitToAppIcon className="icon" />
+                  <span>Logout</span>
                </li>
             </ul>
          </div>
-         <div className="bottom"></div>
+         <div className="bottom">
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+         </div>
       </div>
    )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import './Home.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 const Home = () => {
    return (
       <div className='home' >
-         <h1 className="title">
-            Home page
-         </h1>
+         <Sidebar />
+         <div className="homeContainer">
+            <Navbar />
+            <div className="widgets"></div>
+         </div>
          <Outlet/>
       </div>
    )
